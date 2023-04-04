ddf5871bbf7cc1f18871cbaafb9b7c733b0eda50
Davi Miguel <48900203+davimss@users.noreply.github.com>
2023-04-04 11:39:03 -0300

    Update operadores_logicos_estrutura_condicional.py

--- operadores_logicos_estrutura_condicional.py
+++ operadores_logicos_estrutura_condicional.py
@@ -54,3 +54,24 @@ else:
             print('Ano inválido')
         else:
             print(f'{dia}/{mes}/{ano} é uma data válida')
+            
+# 06. Faça um Programa que leia um número e exiba o dia correspondente da semana. (1-Domingo, 2- Segunda, etc.), se digitar outro valor deve aparecer valor inválido.
+
+print('1-Domingo, 2-Segunda, 3-Terça\n4-Quarta, 5-Quinta, 6-Sexta, 7-Sábado')
+num = int(input('Digite um número de 1 a 7: '))
+if num == 1:
+    print('Domingo')
+elif num == 2:
+    print('Segunda')
+elif num == 3:
+    print('Terça')
+elif num == 4:
+    print('Quarta')
+elif num == 5:
+    print('Quinta')
+elif num == 6:
+    print('Sexta')
+elif num == 7:
+    print('Sábado')
+else:
+    print('Dia inválido')
